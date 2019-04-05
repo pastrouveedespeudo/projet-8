90e2a8da63b1d4dff7f1f542deae8191a470e092
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 15:22:05 +0200

    Add files via upload

---
+++ env/plateforme/tests.py
@@ -0,0 +1,21 @@
+"""Here we testing plateforme app templates"""
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class indexpagetest(TestCase):
+    """Here we testing"""
+
+    def test_home_page(self):
+        """test home template"""
+
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+        
+
+    def test_mention_page(self):
+        """test mention page"""
+
+        response = self.client.get(reverse('mention'))
+        self.assertEqual(response.status_code,200)
