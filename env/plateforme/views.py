4a7123df40bde8763a3343fe2b2eaff6ba17c510
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 15:20:42 +0200

    Add files via upload

---
+++ env/plateforme/views.py
@@ -0,0 +1,25 @@
+"""Here we discuss with home, mention and errors templates"""
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    """Template home"""
+
+    return render(request, "home.html", {})
+
+def mention(request):
+    """template mention"""
+
+    return render(request, "mention.html", {})
+
+def handler404(request, exception, template_name="error.html"):
+    """handler404 error template"""
+
+    return render(request, "error.html", {'message':'Page non trouvée'})
+
+def handler500(request):
+    """handler500 error template"""
+    
+    return render(request, "error.html", {'message':'Erreur 505'})
