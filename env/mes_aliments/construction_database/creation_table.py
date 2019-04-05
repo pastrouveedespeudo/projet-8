bc5b127334f80bac210c5ea0b06bba5261a01169
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 15:07:48 +0200

    Update creation_table.py

--- env/mes_aliments/construction_database/creation_table.py
+++ env/mes_aliments/construction_database/creation_table.py
@@ -7,10 +7,10 @@ class create_tables:
     def create_table_food(self):
         """Here we create food table"""
 
-        conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD) 
 
 
         cur = conn.cursor()
@@ -32,10 +32,10 @@ class create_tables:
     def create_table_substitute(self):
         """we creat food substitute"""
 
-        conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD) 
         cur = conn.cursor()
         cur.execute("""CREATE TABLE mes_aliments_substitut
                     (id serial PRIMARY KEY,
@@ -54,10 +54,10 @@ class create_tables:
     def create_table_category(self):
         """we create table category"""
 
-        conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD) 
 
         cur = conn.cursor()
         cur.execute("CREATE TABLE mes_aliments_categorie\
@@ -70,10 +70,10 @@ class create_tables:
     def create_table_search(self):
         """we create search table"""
 
-        conn = psycopg2.connect(database="dcqhankmnah2r7",
-                                user="nialuqvdnbwagj",
-                                host="ec2-23-23-173-30.compute-1.amazonaws.com",
-                                password="2462155ce31d6d2399f5e161e89355c8a499d2556781657526462dbf67b9ac18")
+        conn = psycopg2.connect(database=DATABASE,
+                                user=USER,
+                                host=HOST,
+                                password=PASSWORD) 
   
         cur = conn.cursor()
         cur.execute("CREATE TABLE mes_aliments_recherche\
