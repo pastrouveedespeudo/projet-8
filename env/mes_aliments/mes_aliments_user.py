518b820f8ee0d1785128936e6e45763fae00d375
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 15:11:33 +0200

    Update mes_aliments_user.py

--- env/mes_aliments/mes_aliments_user.py
+++ env/mes_aliments/mes_aliments_user.py
@@ -1,16 +1,17 @@
 """This is functions for searching from database"""
 
 import psycopg2
+from .config import DATABASE, USER, HOST, PASSWORD
 
 def controle_data_food(username):
     """Here we watch if user have 6 products,
     if he has -6 we ask him to select products
     else we warned him to modify his selection"""
 
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                            user="giervvxxoatsci",
-                            host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                            password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
 
     cur = conn.cursor()
     cur.execute("""SELECT COUNT(name_aliment) FROM aliment_de_{0}
@@ -29,10 +30,10 @@ def controle_data_food(username):
 def insert_food(username, food_name):
     """He we insert food"""
 
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")       
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD)       
     cur = conn.cursor()
     cur.execute("""INSERT INTO aliment_de_{}
                     (name_aliment, username)
