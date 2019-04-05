fc91b06c5230db21ceeecf6f4d821a50c63298c6
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 11:50:31 +0200

    Update urls.py

--- env/mes_aliments/urls.py
+++ env/mes_aliments/urls.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import mes_aliments
-from .views import recherche
-from .views import aliment_det
-from .views import remplacement
+from .views import my_food
+from .views import searching
+from .views import food_det
+from .views import replace
 from .views import error
 
 urlpatterns = [
-    path('/mes_aliments', mes_aliments, name='mes_aliments'),
-    path('/recherche', recherche, name='recherche'),
+    path('/mes_aliments', my_food, name='mes_aliments'),
+    path('/recherche', searching, name='recherche'),
     path('/aliment_det', food_det, name='aliment_det'),
-    path('/remplacement', remplacement, name='remplacement'),
+    path('/remplacement', replace, name='remplacement'),
     path('/error', error, name='error'),
-
     ]
