5b56901e563243926732b897ff8b51f085bbd82d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-05 15:06:20 +0200

    Update config.py

--- env/mes_aliments/config.py
+++ env/mes_aliments/config.py
@@ -1,4 +1,4 @@
-database = "ddgh06joqvm83k"
-user = "giervvxxoatsci"
-host = "ec2-75-101-133-29.compute-1.amazonaws.com"
-password = "2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c"
+DATABASE = "ddgh06joqvm83k"
+USER = "giervvxxoatsci"
+HOST = "ec2-75-101-133-29.compute-1.amazonaws.com"
+PASSWORD = "2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c"
