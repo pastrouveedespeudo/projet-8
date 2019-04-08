9e67086190127881abc5eaf208772431dc3d6458
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-08 10:40:17 +0200

    Update urls.py

--- env/mes_aliments/urls.py
+++ env/mes_aliments/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from .views import my_food
 from .views import searching
 from .views import food_det
-from .views import replace
+from .views import replacing
 from .views import error
 
 urlpatterns = [
     path('/mes_aliments', my_food, name='mes_aliments'),
     path('/recherche', searching, name='recherche'),
     path('/aliment_det', food_det, name='aliment_det'),
-    path('/remplacement', replace, name='remplacement'),
+    path('/remplacement', replacing, name='remplacement'),
     path('/error', error, name='error'),
+
     ]
