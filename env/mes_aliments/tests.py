1a0788d9dbe22de8977267caa43ce54d66c41287
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-12 12:33:56 +0200

    Add files via upload

---
+++ env/mes_aliments/tests.py
@@ -0,0 +1,232 @@
+from django.urls import reverse
+from django.test import TestCase
+from .models import *
+
+class test_page_aliment(TestCase):
+
+    def test_mes_aliments_page(self):        
+        response = self.client.get(reverse('mes_aliments'))
+        self.assertEqual(response.status_code,200)
+
+    def test_recherche_page(self):
+        response = self.client.get(reverse('recherche'))
+        self.assertEqual(response.status_code,200)
+
+
+    def test_aliment_det_page(self):
+        response = self.client.get(reverse('aliment_det'))
+        self.assertEqual(response.status_code,200)
+
+        
+    def test_remplacement_page(self):
+        response = self.client.get(reverse('remplacement'))
+        self.assertEqual(response.status_code,200)
+
+        
+class test_aliment(TestCase):
+    
+    def test_categorie(self):
+        cat = categorie.objects.create(name_categorie='legume')
+
+        category_name = categorie.objects.get(id=1)
+        out = category_name.name_categorie
+        out1 = category_name.id
+        
+        self.assertEqual(out, 'legume')
+        self.assertEqual(out1, 1)
+
+        
+    def test_image(self):
+
+
+        cat = categorie.objects.create(id=1,
+                                       name_categorie='legume')
+        
+        food = aliment.objects.create(name_aliment='carotte',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marché',
+                                      name_brand='marché',
+                                      id_categorie_id=1)
+        
+        food_search = aliment.objects.get(name_aliment='carotte')
+        out = food_search.image
+
+        self.assertEqual(out, 'htps://carotte.com')
+
+
+
+
+
+    def test_title(self):
+        cat = categorie.objects.create(id=1,
+                                       name_categorie='legume')
+        
+        food = aliment.objects.create(name_aliment='carotte',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marché',
+                                      name_brand='marché',
+                                      id_categorie_id=1)
+
+
+
+        out = food.name_aliment
+        self.assertEqual(out, 'carotte')
+
+
+    def test_better_nutri(self):
+        
+        cat = categorie.objects.create(id=1,
+                                       name_categorie='legume')
+        
+        food1 = aliment.objects.create(name_aliment='carotte',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marche',
+                                      name_brand='marche',
+                                      id_categorie_id=1)
+        
+        food2 = aliment.objects.create(name_aliment='tomate',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marche',
+                                      name_brand='marche',
+                                      id_categorie_id=1)
+
+        food3 = aliment.objects.create(name_aliment='olive',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marche',
+                                      name_brand='marche',
+                                      id_categorie_id=1)
+
+        food4 = aliment.objects.create(name_aliment='epinard',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marche',
+                                      name_brand='marche',
+                                      id_categorie_id=1)
+
+
+
+        food5 = aliment.objects.create(name_aliment='cassoulet',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marche',
+                                      name_brand='marche',
+                                      id_categorie_id=1)
+
+
+
+        food6 = aliment.objects.create(name_aliment='champignon',
+                                      code_product_food='4649849',
+                                      description='caroote',
+                                      nutriscore='a',
+                                      image='htps://carotte.com',
+                                      name_store='marche',
+                                      name_brand='marche',
+                                      id_categorie_id=1)
+
+
+        liste = [food1.name_aliment, food1.code_product_food,
+                 food1.description, food1.nutriscore, food1.image,
+                 food1.name_store, food1.name_brand, food1.id_categorie_id,
+                 food2.name_aliment, food2.code_product_food,
+                 food2.description, food2.nutriscore, food2.image,
+                 food2.name_store, food2.name_brand, food2.id_categorie_id,
+                 food3.name_aliment, food3.code_product_food,
+                 food3.description, food3.nutriscore, food3.image,
+                 food3.name_store, food3.name_brand, food3.id_categorie_id,
+                 food4.name_aliment, food4.code_product_food,
+                 food4.description, food4.nutriscore, food4.image,
+                 food4.name_store, food4.name_brand, food4.id_categorie_id,
+                 food5.name_aliment, food5.code_product_food,
+                 food5.description, food5.nutriscore, food5.image,
+                 food5.name_store, food5.name_brand, food5.id_categorie_id,
+                 food6.name_aliment, food6.code_product_food,
+                 food6.description, food6.nutriscore, food6.image,
+                 food6.name_store, food6.name_brand, food6.id_categorie_id,
+                 ]
+
+        out = liste
+        self.assertEqual(out, (['carotte', '4649849',
+                               'caroote','a','htps://carotte.com',
+                               'marche', 'marche',1,
+                               'tomate', '4649849', 'caroote',
+                               'a','htps://carotte.com', 'marche', 'marche',
+                               1,'olive', '4649849', 'caroote',
+                               'a','htps://carotte.com', 'marche', 'marche',
+                               1,'epinard', '4649849', 'caroote',
+                               'a','htps://carotte.com', 'marche', 'marche',
+                               1,'cassoulet', '4649849', 'caroote',
+                               'a','htps://carotte.com', 'marche', 'marche',
+                               1,'champignon', '4649849', 'caroote',
+                               'a','htps://carotte.com', 'marche', 'marche',
+                               1]
+
+                            ))
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
