35054ffad87368ccd6922293342f7f509ea1caa9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-15 11:08:26 +0200

    Update and rename recherche1.js to la_recherche1_js.js

--- env/static/js/la_recherche1_js.js
+++ env/static/js/la_recherche1_js.js
@@ -2,12 +2,15 @@ var LISTE = ["a"];
 var LISTE_NAME = [];
 var USER = ["b"];
 
+
 $("#product1,#product2,#product3,#product4,#product5,#product6").on("click", function(e){
    e.preventDefault();
+
     $.ajax({
         data:{
             'data[]':LISTE[LISTE.length - 1],
             'username':USER[USER.length - 1],
+     
         },
         type:"POST",
         url:"/mes_aliments/recherche/"
@@ -15,47 +18,61 @@ $("#product1,#product2,#product3,#product4,#product5,#product6").on("click", fun
     .done(function(data){
         if (data.error){
             $("#monCadreAlert").text(data.error);
-            $("#is_save");   
+            $("#is_save");
+            
+            
         }
         else{
             $("#is_save").html(data.data);
             $("#monCadreAlert");
+            
+            
         };
+        
     });
+
 });
 
 
+
+
 function pushlist(id_product, user, stock, is_save){
+  
   var a = document.getElementById(id_product).value;
   var b = document.getElementById(id_product).name;
   var c = document.getElementById(user).innerHTML;
   USER.push(c)
   LISTE.push(a)
   LISTE_NAME.push(b)
+  
   d = document.getElementById(stock).innerHTML;
   console.log(d)
-  if (d == "oups vous avez trop d'aliment en stock supprime en ! ou remplace le !"){
+
+  if (d == "<center><strong>oups vous avez trop d'aliment en stock supprime en ! ou remplace le !</strong></center>"){
       console.log("trop d\'aliment pour ce compte")
       document.getElementById(is_save).innerHTML = "";
       document.getElementById(is_save).innerHTML = '<img style="width:15%;" src="/static/img/portfolio/recherche/croix.jpg" />'
-      +'  <i>Vous avez trop d\'alement</i>';
-    }
-    else{
-        document.getElementById(is_save).innerHTML = "";
-        document.getElementById(is_save).innerHTML = '<img style="width:15%;" src="/static/img/portfolio/recherche/validate.jpg" />'
-        +'  <i>Enregistrement effectué</i>';
-    };
+  +'  <i>Vous avez trop d\'alement</i>';
+  }
+  else{
+      document.getElementById(is_save).innerHTML = "";
+      document.getElementById(is_save).innerHTML = '<img style="width:15%;" src="/static/img/portfolio/recherche/validate.jpg" />'
+  +'  <i>Enregistrement effectuee</i>';
+  };
   };
   
+
 
 
 var c = document.getElementById("user").innerHTML;
 USER.push(c)
 console.log(USER)
 
+
 $.ajax({
     data:{
         'username':USER[USER.length - 1],
+ 
     },
     type:"POST",
     url:"/mes_aliments/recherche/"
@@ -64,11 +81,15 @@ $.ajax({
     if (data.error){
         $("#monCadreAlert").text(data.error);
         $("#stock");
+        
+        
     }
     else{
         $("#stock").html(data.data);
-        $("#monCadreAlert"); 
-    }; 
+        $("#monCadreAlert");
+        
+        
+    };
+    
 });
-
 
