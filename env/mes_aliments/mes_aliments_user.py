a5c518cb0c38147ca1c92a71c1e90ef5646017e2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-17 19:37:26 +0200

    Update mes_aliments_user.py

--- env/mes_aliments/mes_aliments_user.py
+++ env/mes_aliments/mes_aliments_user.py
@@ -1,41 +1,45 @@
-"""This is functions for searching from database"""
+"""Here we discuss with database for
+food from user database"""
 
-import psycopg2
-from .config import DATABASE, USER, HOST, PASSWORD
 
-def controle_data_food(username):
-    """Here we watch if user have 6 products,
-    if he has -6 we ask him to select products
-    else we warned him to modify his selection"""
+from accounts.models import *
+from mes_aliments.models import *
 
-    conn = psycopg2.connect(database=DATABASE,
-                            user=USER,
-                            host=HOST,
-                            password=PASSWORD) 
 
-    cur = conn.cursor()
-    cur.execute("""SELECT COUNT(name_aliment) FROM aliment_de_{0}
-                    """.format(username))
-    conn.commit()
-    rows = cur.fetchall()
-    count_food = [i for i in rows]
+def my_food_user(username):
+    """Here we take user food"""
 
-    if count_food[0][0] >= 6:
-        return "nombre de produit suppérieur a 6", False
+    liste = []
+    c = foodAccount.objects.get(name=username)
+    food = [c.name_aliment1, c.name_aliment2, c.name_aliment3,
+            c.name_aliment4, c.name_aliment5, c.name_aliment6]
+    for i in food:
+        print(i)
+        liste.append(i)
+        
+    with open("mimi.py","w") as file:
+        file.write(str(liste))
+        print("fait")
+        print(liste)
+    return food
+
+def display_food(liste_aliment):
+    """Here we take informations food for template"""
+
+    liste_ali = []
+    try:
+        for i in liste_aliment:
+             z = aliment.objects.get(name_aliment=i)
+             liste = []
+             liste = [z.name_aliment, z.code_product_food,
+                      z.description, z.nutriscore,
+                      z.image, z.name_store, z.name_brand]
+
+             liste_ali.append(liste)
+     
+
+        return liste_ali
     
-    else:
-        return "stockage du produit possible", True
-
-
-def insert_food(username, food_name):
-    """He we insert food"""
-
-    conn = psycopg2.connect(database=DATABASE,
-                            user=USER,
-                            host=HOST,
-                            password=PASSWORD)       
-    cur = conn.cursor()
-    cur.execute("""INSERT INTO aliment_de_{}
-                    (name_aliment, username)
-                    VALUES (LOWER('{}'), '{}');""".format(username, food_name, username))
-    conn.commit()
+    except:
+        pass
+     
