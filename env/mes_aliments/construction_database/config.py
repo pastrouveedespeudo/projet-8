334776f85b7a7aa5892d1e091788605ca7b97534
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-01 22:00:46 +0200

    Update config.py

--- env/mes_aliments/construction_database/config.py
+++ env/mes_aliments/construction_database/config.py
@@ -1,4 +1,4 @@
-DATABASE = "ddgh06joqvm83k"
-USER = "giervvxxoatsci"
-HOST = "ec2-75-101-133-29.compute-1.amazonaws.com"
-PASSWORD = "2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c"
+DATABASE = "plateforme"
+USER = "jb"
+HOST = "127.0.0.1"
+PASSWORD = "tiotiotio333"
