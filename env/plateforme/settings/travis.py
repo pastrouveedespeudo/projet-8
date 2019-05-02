26a2e7cfaf31d4acaa7e833506892a3a7ca9a940
jb <jb26400@hotmail.fr>
2019-05-02 11:15:24 +0000

    gogogogogo

---
+++ env/plateforme/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'plateforme',
+        'USER': 'postgres',
+        'PASSWORD': 'tiotiotio333',
+        'HOST': '157.230.34.183',
+        'PORT': '5432',
+    },
+}i
