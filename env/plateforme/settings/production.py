49e3b57402c34240cf5bde8b4f6b1bfc3e9360ad
jb <jb26400@hotmail.fr>
2019-05-02 13:27:38 +0000

    add travis

---
+++ env/plateforme/settings/production.py
@@ -0,0 +1,18 @@
+from . import *
+
+SECRET_KEY = '_7cd!+a@ci9f93a%lb3#-a(brz2zblelg=1qjlc!b_*g_@(mxk'
+
+DEBUG = False
+ALLOWED_HOSTS = ['157.230.34.183']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': 'plateforme', # le nom de notre base de données créée précédemment
+        'USER': 'jb', # attention : remplacez par votre nom d'utilisateur !!
+        'PASSWORD': 'tiotiotio333',
+        'HOST': '',
+        'PORT': '5432',
+    }
+}
+
