778e6fa191839f8182b6e229a0805dc93bbd2c5a
jb <jb26400@hotmail.fr>
2019-05-03 10:29:37 +0000

    bon spas ca

--- env/plateforme/settings/__init__.py
+++ env/plateforme/settings/__init__.py
@@ -80,14 +80,7 @@ TEMPLATES = [
 
 
 
-TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.Loader',
-	'django.template.loaders.app_directories.Loader',
-)
 
-TEMPLATE_DIRS = (
-	os.path.join(BASE_DIR, 'templates'),
-)
 
 WSGI_APPLICATION = 'plateforme.wsgi.application'
 
