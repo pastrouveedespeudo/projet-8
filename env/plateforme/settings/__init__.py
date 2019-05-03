fab57853d0940e0079beea2ce8c3eeea5a7babf5
jb <jb26400@hotmail.fr>
2019-05-03 10:24:18 +0000

    crochet en trop

--- env/plateforme/settings/__init__.py
+++ env/plateforme/settings/__init__.py
@@ -65,7 +65,7 @@ TEMPLATE_DIRS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [[os.path.join(BASE_DIR, "templates")], 
+        'DIRS': [os.path.join(BASE_DIR, "templates")], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
