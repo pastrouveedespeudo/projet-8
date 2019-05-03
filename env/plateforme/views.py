21e4d2241afa6ba861b6d146f75bbe2d91bd9d8d
jb <jb26400@hotmail.fr>
2019-05-03 13:34:45 +0000

    creation erreur STAGING1

--- env/plateforme/views.py
+++ env/plateforme/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 def home(request):
     """Template home"""
-
+	fjpezfpojzepf
     return render(request, "home.html", {})
 
 def mention(request):
