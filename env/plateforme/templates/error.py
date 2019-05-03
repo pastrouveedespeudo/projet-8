ab82bfaaf935a0d732d9e61ee642e588ecf0b60e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-03 14:15:42 +0200

    Update error.py

--- env/plateforme/templates/error.py
+++ env/plateforme/templates/error.py
@@ -2,34 +2,17 @@
 
 {% block content %}
 {% endblock %}
+{% load static %}
+
+<link href="{% static 'css/bottom.css' %}" rel="stylesheet">
+
 <div id="msg">
-<br><br>
-<center><div><strong>{{message}}</strong></div></center>
-<br><br>
+   <br><br>
+   <center><div><strong>{{message}}</strong></div></center>
+   <br><br>
 </div>
- <header class="masthead text-center d-flex">
 
-
-
-<style>
-    header.masthead {
-
-    padding-top: 10rem;
-    padding-bottom: calc(10rem - 56px);
-    background-image: url("/static/img/portfolio/recherche/oups.jpg");
-    background-position: center center;
-    background-size: cover;
-    }
-    #msg{
-
-        background-color:rgba(255, 200, 102);
-
-    }
-
-
-</style>
-
-    
-    <img src="/static/img/errir2.png" style="margin-right:auto;" >
-    <img src="/static/img/error1.png" >
+<header class="masthead text-center d-flex">
+    <img src="/static/img/errir2.png" style="margin-right:auto;">
+    <img src="/static/img/error1.png">
 </header>
